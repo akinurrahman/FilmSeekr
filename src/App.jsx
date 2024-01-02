64345a36f6d612d5b9e3c5ac12775cc4ee2d7002
Akinur Rahman <akinurrahmanofficial@gmail.com>
2024-01-02 22:51:26 +0530

    react router setup done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
 import React from "react";
-import { useTrendingMoviesQuery } from "./api/fetchTmdb";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/home/Home";
+import Details from "./pages/details/Details";
+import SearchResults from "./pages/search results/SearchResults";
+import Explore from "./pages/explore/Explore";
+import PageNotFound from "./pages/Not Found/PageNotFound";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
 
 const App = () => {
-  const { data } = useTrendingMoviesQuery("/trending/movie/week");
-  console.log(data)
-  return <div></div>;
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/:mediaType/:id" element={<Details />} />
+        <Route path="/search/:query" element={<SearchResults />} />
+        <Route path="/explore/:mediaType" element={<Explore />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
 };
 
 export default App;
