d2ae66e33f01aa41ae147ea107cf2db25502eba7
Akinur Rahman <akinurrahmanofficial@gmail.com>
2024-01-07 15:25:46 +0530

    header done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,39 +5,82 @@ import { VscChromeClose } from "react-icons/vsc";
 
 const Header = () => {
   const [menu, setMenu] = useState(false);
+  const [searchField, setSearchField] = useState(false);
+
+  const openSearch = () => {
+    setMenu(false);
+    setSearchField((prev) => !prev);
+  };
+
+  const openMobileMenu = () => {
+    setMenu((prev) => !prev);
+    setSearchField(false);
+  };
+
   return (
-    <header className="fixed z-10 mx-auto flex h-[60px] w-full translate-y-0 transform items-center justify-between  bg-black bg-opacity-65 px-5 sm:px-8 md:px-12 lg:px-16">
-      {/* Column 1 : Logo */}
-      <section>
-        <img
-          src="/assets/logo-yello.png"
-          alt="logo"
-          className="h-[40px] cursor-pointer"
-        />
+    <header className="fixed z-10 w-full">
+      <section className="mx-auto  flex h-[60px]  items-center justify-between  bg-black bg-opacity-40 px-5 sm:px-8 md:px-12 lg:px-16">
+        {/* Column 1 : Logo */}
+        <section>
+          <img
+            src="/assets/logo-yello.png"
+            alt="logo"
+            className="h-[40px] cursor-pointer"
+          />
+        </section>
+
+        {/* Column 2: Hamburger icons (visible in mobile view) */}
+        <section className={`flex items-center space-x-4 md:hidden`}>
+          <HiOutlineSearch
+            className="cursor-pointer text-2xl text-white"
+            onClick={openSearch}
+          />
+          <SlMenu
+            className={`cursor-pointer text-2xl text-white ${
+              !menu ? "block" : "hidden"
+            }`}
+            onClick={openMobileMenu}
+          />
+          <VscChromeClose
+            className={`cursor-pointer text-2xl text-white ${
+              menu ? "block" : "hidden"
+            }`}
+            onClick={openMobileMenu}
+          />
+        </section>
+
+        {/* Column 3: Navigation (visible from md breakpoint and above) */}
+        <nav className={`hidden text-white md:block`}>
+          <ul className="flex items-center space-x-5 ">
+            <li>Movies</li>
+            <li>TV Shows</li>
+            <li>
+              <HiOutlineSearch size={20} onClick={openSearch} />
+            </li>
+          </ul>
+        </nav>
       </section>
 
-      {/* Column 2: Hamburger icons (visible in mobile view) */}
-      <section className={`flex items-center space-x-4 md:hidden`}>
-        <HiOutlineSearch className="text-2xl text-white" />
-        <SlMenu
-          className={`text-2xl text-white ${!menu ? "block" : "hidden"}`}
-          onClick={() => setMenu((prev) => !prev)}
+      {/* show only when menu state is true */}
+      <section className={`${menu ? "block" : "hidden"} `}>
+        <ul className="bg-black bg-opacity-40 text-white">
+          <li className="mx-5 py-3  text-xl font-medium">Movies</li>
+          <li className="mx-5 py-3 text-xl  font-medium">TV Shows</li>
+        </ul>
+      </section>
+
+      {/* Section visible when searchField state is true */}
+      <section className={`relative  ${searchField ? "block" : "hidden"}`}>
+        <input
+          type="text"
+          className="w-full px-4 pr-[60px] py-3 outline-none"
+          placeholder="Search for a movie or a tv show"
         />
         <VscChromeClose
-          className={`text-2xl text-white ${menu ? "block" : "hidden"}`}
-          onClick={() => setMenu((prev) => !prev)}
+          size={20}
+          className="absolute right-6 top-4   cursor-pointer"
+          onClick={openSearch}
         />
-      </section>
-
-      {/* Column 3: Navigation (visible from md breakpoint and above) */}
-      <section className={`hidden text-white md:block`}>
-        <ul className="flex items-center space-x-5 ">
-          <li>Movies</li>
-          <li>TV Shows</li>
-          <li className="">
-            <HiOutlineSearch size={20} />
-          </li>
-        </ul>
       </section>
     </header>
   );
