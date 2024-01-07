92aa6ba3b74723277f17a65db633b2ffd2253f1f
Akinur Rahman <akinurrahmanofficial@gmail.com>
2024-01-07 12:52:45 +0530

    normal header done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,46 @@
-import React from 'react'
+import React, { useState } from "react";
+import { HiOutlineSearch } from "react-icons/hi";
+import { SlMenu } from "react-icons/sl";
+import { VscChromeClose } from "react-icons/vsc";
 
 const Header = () => {
+  const [menu, setMenu] = useState(false);
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <header className="fixed z-10 mx-auto flex h-[60px] w-full translate-y-0 transform items-center justify-between  bg-black bg-opacity-65 px-5 sm:px-8 md:px-12 lg:px-16">
+      {/* Column 1 : Logo */}
+      <section>
+        <img
+          src="/assets/logo-yello.png"
+          alt="logo"
+          className="h-[40px] cursor-pointer"
+        />
+      </section>
 
-export default Header
+      {/* Column 2: Hamburger icons (visible in mobile view) */}
+      <section className={`flex items-center space-x-4 md:hidden`}>
+        <HiOutlineSearch className="text-2xl text-white" />
+        <SlMenu
+          className={`text-2xl text-white ${!menu ? "block" : "hidden"}`}
+          onClick={() => setMenu((prev) => !prev)}
+        />
+        <VscChromeClose
+          className={`text-2xl text-white ${menu ? "block" : "hidden"}`}
+          onClick={() => setMenu((prev) => !prev)}
+        />
+      </section>
+
+      {/* Column 3: Navigation (visible from md breakpoint and above) */}
+      <section className={`hidden text-white md:block`}>
+        <ul className="flex items-center space-x-5 ">
+          <li>Movies</li>
+          <li>TV Shows</li>
+          <li className="">
+            <HiOutlineSearch size={20} />
+          </li>
+        </ul>
+      </section>
+    </header>
+  );
+};
+
+export default Header;
