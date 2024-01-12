dfab65ca7f08c882af4fea4c414b2dc0db9bb9f3
Akinur Rahman <akinurrahmanofficial@gmail.com>
2024-01-12 22:40:29 +0530

    genres done

--- src/components/Genres.jsx
+++ src/components/Genres.jsx
@@ -2,26 +2,28 @@ import React from "react";
 import { useSelector } from "react-redux";
 
 const Genres = ({ data }) => {
-  // const data = [18, 80];
-
   const { allGenres } = useSelector((state) => state.genres);
+
+  // Filtering unique genres based on their IDs
   const uniqueGenres = allGenres.filter(
     (obj, index, array) => array.findIndex((o) => o.id === obj.id) === index,
   );
 
+  // Filtering genres based on the provided data
   const filteredGenres = uniqueGenres?.filter((currElem) => {
     return data.includes(currElem.id);
   });
-  console.log(filteredGenres);
 
   return (
     <div
-      className={`relative bottom-[81px] float-end flex gap-1 pr-2 text-[12px] ${
-        filteredGenres.length > 0 ? "flex-wrap" : ""
-      }`}
+      className={`relative bottom-[81px] float-end hidden w-[60%] justify-end gap-[2px] pr-2 text-[12px] md:flex`}
     >
+      {/* Mapping through filtered genres and rendering them */}
       {filteredGenres?.map((genre) => (
-        <span key={genre.name} className="rounded-sm bg-pink-600 px-1">
+        <span
+          key={genre.name}
+          className="line-clamp-1 w-1/2 rounded-sm bg-pink-600 pl-1"
+        >
           {genre.name}
         </span>
       ))}
