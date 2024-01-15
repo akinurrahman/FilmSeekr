1857d042391d74effb2f46b2181e54d31d8444d7
Akinur Rahman <akinurrahmanofficial@gmail.com>
2024-01-15 12:01:26 +0530

    switch tab: fixed active tab

--- src/components/SwitchTabs.jsx
+++ src/components/SwitchTabs.jsx
@@ -1,25 +1,29 @@
+// SwitchTabs.jsx
 import React, { useState } from "react";
 
 const SwitchTabs = ({ data, onTabChange }) => {
-  const [activeTab, setActiveTab] = useState("Day");
+  const [activeTab, setActiveTab] = useState(0);
 
-  const handleTabClick = (tab) => {
-    setActiveTab(tab);
-    onTabChange(tab);
+  const handleTabClick = (index) => {
+    setActiveTab(index);
+    onTabChange(data[index]);
   };
 
   return (
     <section className="switch-tabs">
-      <div className="tab-items cursor-pointer rounded-full  bg-white  px-1 py-2 text-black">
+      <div className="tab-items cursor-pointer rounded-full bg-white px-1 py-2 text-black">
         {data.map((tab, index) => (
           <span
             key={index}
-            className={` ${
-              activeTab === tab
-                ? "rounded-full bg-gradient-to-r from-yellow-200 to-yellow-500 px-5 py-1 "
-                : "px-5 py-1"
-            }`}
-            onClick={() => handleTabClick(tab)}
+            className={` 
+              cursor-pointer rounded-full px-5 py-1
+              ${
+                index === activeTab
+                  ? "bg-gradient-to-r from-yellow-200 to-yellow-500 text-[#64647f]"
+                  : "bg-white text-black"
+              }
+            `}
+            onClick={() => handleTabClick(index)}
           >
             {tab}
           </span>
