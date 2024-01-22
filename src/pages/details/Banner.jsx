27e86090249c9b68c1f058540394ab32161bd8f6
Akinur Rahman <akinurrahmanofficial@gmail.com>
2024-01-22 19:56:52 +0530

    details banner: poster done

--- src/pages/details/Banner.jsx
+++ src/pages/details/Banner.jsx
@@ -1,13 +1,22 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
+import React from "react";
+import { useParams } from "react-router-dom";
+import { useFetchTBDBQuery } from "../../api/fetchMovies";
+import { generateTMDBImageUrl } from "../../utility/generateTMDBImageUrl";
+import noPoster from "../../assets/no-poster.png";
+import Img from "../../components/LazyLoadImg";
 
 const Banner = () => {
-    const {} = useParams()
+  const { mediaType, id } = useParams();
+  const { data } = useFetchTBDBQuery(`${mediaType}/${id}`);
+  const poster = generateTMDBImageUrl(data?.poster_path, "w500") || noPoster;
   return (
-    <div className='min-h-[700px]'>
-      
-    </div>
-  )
-}
+    <section className="mx-auto min-h-[700px] max-w-[1200px] md:flex">
+      <div className=" px-[22px] py-[90px] ">
+        <Img src={poster} className={"w-full rounded-2xl"} />
+      </div>
+      <div className="details__block">something</div>
+    </section>
+  );
+};
 
-export default Banner
+export default Banner;
