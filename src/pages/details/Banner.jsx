8834e1cf608700da394dfea7165ff87c49635a0a
Akinur Rahman <akinurrahmanofficial@gmail.com>
2024-01-22 20:18:43 +0530

    title and genres done

--- src/pages/details/Banner.jsx
+++ src/pages/details/Banner.jsx
@@ -9,12 +9,30 @@ const Banner = () => {
   const { mediaType, id } = useParams();
   const { data } = useFetchTBDBQuery(`${mediaType}/${id}`);
   const poster = generateTMDBImageUrl(data?.poster_path, "w500") || noPoster;
+  const title = data?.title || "";
+  const overview = data?.overview || "";
+  const status = data?.status || "";
+  const releaseDate = data?.release_date || "";
+  const genres = data?.genres || [];
+  console.log(genres);
   return (
     <section className="mx-auto min-h-[700px] max-w-[1200px] md:flex">
-      <div className=" px-[22px] py-[90px] ">
+      <div className=" px-[22px] pt-[90px] ">
         <Img src={poster} className={"w-full rounded-2xl"} />
       </div>
-      <div className="details__block">something</div>
+      <div className="details__block mt-5 px-[22px]">
+        <h2 className="text-3xl  mb-2">{title}</h2>
+        <div className=" flex items-center ">
+          {genres?.map((g, index) => (
+            <span
+              className="mx-[2px] rounded-md bg-pink-700 px-1 text-sm"
+              key={index}
+            >
+              {g.name}
+            </span>
+          ))}
+        </div>
+      </div>
     </section>
   );
 };
