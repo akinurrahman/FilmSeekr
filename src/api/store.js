40ecedbf7d43bf77b79ba3a87c96a0eb1dfe4cc2
Akinur Rahman <akinurrahmanofficial@gmail.com>
2024-02-07 10:26:32 +0530

    delete: rtk query files

--- src/api/store.js
+++ src/api/store.js
@@ -1,14 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { fetchMovies } from "./fetchMovies";
 import genresReducer from "./slices/genresSlice";
 
 const store = configureStore({
   reducer: {
     genres: genresReducer,
-    [fetchMovies.reducerPath]: fetchMovies.reducer,
   },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(fetchMovies.middleware),
 });
 
 export default store;
